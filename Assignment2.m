2475652865355c5cf78f0caabcbb3678c4ec3066
gdisumma <gdisumma@student.tudelft.nl>
2023-03-06 14:58:33 +0100

    Question C done

--- Assignment2.m
+++ Assignment2.m
@@ -5,40 +5,40 @@ clear all
 %Read data 1
 T1 = readtable('IAGOS_timeseries_2019021011295591','Delimiter',' '); 
 %Distance is in km
-[totaldistance1,nonLTOdistance1] = questionA(T1);
+[totaldistance1,nonLTOdistance1,tempcon1] = questionA(T1);
 
 %Read data 2
 T2 = readtable('IAGOS_timeseries_2019021102051591','Delimiter',' '); 
 %Distance is in km
-[totaldistance2,nonLTOdistance2] = questionA(T2);
+[totaldistance2,nonLTOdistance2,tempcon2] = questionA(T2);
 
 %Read data 3
 T3 = readtable('IAGOS_timeseries_2019021122212591','Delimiter',' '); 
 %Distance is in km
-[totaldistance3,nonLTOdistance3] = questionA(T3);
+[totaldistance3,nonLTOdistance3,tempcon3] = questionA(T3);
 
 %Read data 4
 T4 = readtable('IAGOS_timeseries_2019021216295591','Delimiter',' '); 
 %Distance is in km
-[totaldistance4,nonLTOdistance4] = questionA(T4);
+[totaldistance4,nonLTOdistance4,tempcon4] = questionA(T4);
 
 %Read data 5
 T5 = readtable('IAGOS_timeseries_2019042914412591','Delimiter',' '); 
 %Distance is in km
-[totaldistance5,nonLTOdistance5] = questionA(T5);
+[totaldistance5,nonLTOdistance5,tempcon5] = questionA(T5);
 
 %Read data 6
 T6 = readtable('IAGOS_timeseries_2019043004153591','Delimiter',' '); 
 %Distance is in km
-[totaldistance6,nonLTOdistance6] = questionA(T6);
+[totaldistance6,nonLTOdistance6,tempcon6] = questionA(T6);
 
 %Read data 7
 T7 = readtable('IAGOS_timeseries_2019043020424591','Delimiter',' '); 
 %Distance is in km
-[totaldistance7,nonLTOdistance7] = questionA(T7);
+[totaldistance7,nonLTOdistance7,tempcon7] = questionA(T7);
 
 %Read data 8
 T8 = readtable('IAGOS_timeseries_2019050116041591','Delimiter',' '); 
 %Distance is in km
-[totaldistance8,nonLTOdistance8] = questionA(T8);
+[totaldistance8,nonLTOdistance8,tempcon8] = questionA(T8);
 
--- questionA.m
+++ questionA.m
@@ -1,5 +1,7 @@
-function[totaldistance,nonLTOdistance] = questionA(T)
+function[totaldistance,nonLTOdistance,TempCond] = questionA(T)
 
+
+Th = -38+273.15; %Kelvin
 len = size(T);
 len = len(:,1);
 count = 1;
@@ -7,12 +9,12 @@ count = 1;
 
 totlist = [];
 nonLTOlist = [];
+templist = [];
 
 for i = drange(73:len)
    flag1 = T(i,15).Var15;
    if flag1 ~= 0
        count = count + 1;
-       t2 = T(i,1).Var1;
        continue
    end
    t1 = T(i-count,1).Var1;
@@ -29,9 +31,16 @@ for i = drange(73:len)
    if pres <=750
        nonLTOlist = [nonLTOlist dist];
    end
+   temp = T(i,16).Var16;
+   if temp <= Th
+       templist = [templist dist];
+   end
 end
 
 totaldistance = sum(totlist);
 totaldistance = totaldistance/1000;
 nonLTOdistance = sum(nonLTOlist);
 nonLTOdistance = nonLTOdistance/1000;
+tempdistance = sum(templist);
+tempdistance = tempdistance/1000;
+TempCond = (tempdistance/nonLTOdistance)*100;
