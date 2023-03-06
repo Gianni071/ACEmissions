d87e4e7fa1d0ded3cb3bee2f315e347da43c2d21
gdisumma <gdisumma@student.tudelft.nl>
2023-03-06 13:03:43 +0100

    Added all text files and finished question A

--- Assignment2.m
+++ Assignment2.m
@@ -2,6 +2,14 @@ clc
 close all
 clear all
 
-T = readtable('IAGOS_timeseries_2019021011295591','Delimiter',' '); 
+%Read data 1
+T1 = readtable('IAGOS_timeseries_2019021011295591','Delimiter',' '); 
 
+%Distance is in km
+[totaldistance1,nonLTOdistance1] = questionA(T1);
 
+%Read data 2
+T2 = readtable('IAGOS_timeseries_2019021011295591','Delimiter',' '); 
+
+%Distance is in km
+[totaldistance2,nonLTOdistance2] = questionA(T2);
---
+++ questionA.m
@@ -0,0 +1,38 @@
+function[totaldistance,nonLTOdistance] = questionA(T)
+
+len = size(T);
+len = len(:,1);
+count = 1;
+
+
+totlist = [];
+nonLTOlist = [];
+
+for i = drange(73:len)
+   flag1 = T(i,15).Var15;
+   t1 = T(i,1).Var1;
+   if flag1 ~= 0
+       count = count + 1;
+       t2 = T(i,1).Var1;
+       continue
+   end
+   t1 = T(i-count,1).Var1;
+   t2 = T(i,1).Var1;
+   v1 = T(i-count,14).Var14;
+   v2 = T(i,14).Var14;
+   count = 1;
+   vbar = (v1+v2)/2;
+   dt = t2-t1;
+   dist = vbar*dt;
+   totlist = [totlist dist];
+   pres = T(i,10).Var10;
+   pres = pres/100;
+   if pres <=750
+       nonLTOlist = [nonLTOlist dist];
+   end
+end
+
+totaldistance = sum(totlist);
+totaldistance = totaldistance/1000;
+nonLTOdistance = sum(nonLTOlist);
+nonLTOdistance = nonLTOdistance/1000;
